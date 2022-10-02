93e07c4c492b213f26661802e0178b1d6430ce2e
JShipov <107051542+JShipov@users.noreply.github.com>
2022-10-02 17:51:05 +0300

    Update Main.java

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,24 +3,34 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //get the console input for first number
         Scanner scanner = new Scanner(System.in);
         System.out.println("First number:");
 
+        //checking input for character other than numbers
+        //loop won`t end until get a number
         while (!scanner.hasNextDouble()) {
             System.out.println("It`s not a number.");
             scanner.nextLine();
         }
-        System.out.print("Second number: ");
+        
         double firstNumber = scanner.nextDouble();
+        
+        //get the console input for second
+        System.out.print("Second number: ");
 
-
+        //checking input for character other than numbers
+        //loop won`t end until get a number
         while (!scanner.hasNextDouble()) {
             System.out.println("It`s not a number.");
             scanner.nextLine();
         }
         double secondNumber = scanner.nextDouble();
-
+           
         new Calculator(firstNumber, secondNumber);
+        
+        //choose operatiom among simple calculations
+        //call the method for the choosen action
         System.out.println("Please, select (*,/,+,-)");
         String message = "Selected: ";
         boolean repeat;
